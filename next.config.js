1fd736997a13155f6da0cc8d51ade68216b20816
doandevhere <tranduongdoan03122001@gmail.com>
2024-08-21 17:18:33 +0700

    chore: don't open automatic  analyzer packages

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withNextIntl = createNextIntlPlugin('./i18n/i18n.ts');
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: false,
 });
 
 /** @type {import('next').NextConfig} */
